82d1b34cf26530abed51364d6fdfb62ecef82982
hancher <hanchers@outlook.com>
2025-06-09 16:04:38 +0800

    初始化dag数据库

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO user (name, email) VALUES ('Alice', 'alice@example.com');
-INSERT INTO user (name, email) VALUES ('Bob', 'bob@example.com');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,30 @@
-CREATE TABLE IF NOT EXISTS user (
+
+-- 服务节点表
+CREATE TABLE IF NOT EXISTS service_node (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL
+    name TEXT NOT NULL, -- 服务节点名称
+    remark TEXT, -- 备注
+    status TEXT NOT NULL default 'down', -- 节点状态，枚举：down-不可用、wait-等待依赖恢复、ok-可用
+    cluster_id INTEGER NOT NULL, -- 服务集群唯一标识，控制依赖的基本单位
+    health_check_method TEXT NOT NULL, -- 健康检查方法、类型,枚举:http、bash
+    health_check_cmd TEXT, -- 健康检查命令
+    restart_method TEXT NOT NULL, -- 重启类型、方法,枚举:bash
+    restart_cmd TEXT -- 重启命令
+);
+
+-- 服务集群表
+CREATE TABLE IF NOT EXISTS service_cluster (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL, -- 服务节点名称
+    remark TEXT, -- 备注
+    status TEXT NOT NULL default 'down', -- 节点状态，枚举：down-不可用、up-达到最小存活数量、ok-全部服务可用、wait-等待依赖恢复、
+    min_alive_num INTEGER NOT NULL default 1 -- 服务集群最小存活数量
+);
+
+-- 依赖的有向无环图
+CREATE TABLE IF NOT EXISTS dependent_dag (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    source_cluster_id TEXT NOT NULL default 'start',
+    target_cluster_id TEXT NOT NULL default 'end',
+    UNIQUE(source_cluster_id, target_cluster_id)
 );
